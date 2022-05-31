43d5ba096c12a6c1e4b6b9a4269280cae56811b4
Josh Bailey <josh@vandervecken.com>
2022-05-31 19:15:45 +0000

    Update protobuf lib to v4 and re-generate definitions.

--- faucetconfrpc/generate_pb_grpc.sh
+++ faucetconfrpc/generate_pb_grpc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-protoc protos/faucetconfrpc/faucetconfrpc.proto --go_out=plugins=grpc:.
+protoc protos/faucetconfrpc/faucetconfrpc.proto --go-grpc_out=. --go_out=.
 python3 -m grpc_tools.protoc -Iprotos protos/faucetconfrpc/faucetconfrpc.proto --python_out=.. --grpc_python_out=..
