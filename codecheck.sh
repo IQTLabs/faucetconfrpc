58c1570f5936a593af770ef4ba73c50f767cee19
Josh Bailey <josh@vandervecken.com>
2022-07-12 09:56:08 +0000

    move back from src for golang compat.

--- codecheck.sh
+++ codecheck.sh
@@ -3,5 +3,5 @@
 set -e
 
 # https://github.com/google/pytype/issues/764 (disable pyi-error for protobuf)
-PYTHONPATH=$pwd/src/faucetconfrpc:$PYTHONPATH pytype --disable=pyi-error --exclude=src/faucetconfrpc/faucetconfrpc_pb2_grpc.py --exclude=src/faucetconfrpc/faucetconfrpc_pb2.py src/faucetconfrpc/*py
-pylint --ignore=faucetconfrpc_pb2_grpc.py,faucetconfrpc_pb2.py src/faucetconfrpc/*py
+PYTHONPATH=$pwd/faucetconfrpc:$PYTHONPATH pytype --disable=pyi-error --exclude=faucetconfrpc/faucetconfrpc_pb2_grpc.py --exclude=faucetconfrpc/faucetconfrpc_pb2.py faucetconfrpc/*py
+pylint --ignore=faucetconfrpc_pb2_grpc.py,faucetconfrpc_pb2.py faucetconfrpc/*py
