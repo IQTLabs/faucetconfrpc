0713ff1e5fad1c10855a709a90b009755e173009
Josh Bailey <josh@vandervecken.com>
2022-10-12 09:57:23 +1300

    move to poetry from setuptools.

--- tests/codecheck.sh
+++ tests/codecheck.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-PYTHONPATH=$pwd/src/faucetconfrpc:$PYTHONPATH pytype --exclude=src/faucetconfrpc/faucetconfrpc_pb2_grpc.py --exclude=src/faucetconfrpc/faucetconfrpc_pb2.py src/faucetconfrpc/*py
-pylint --ignore=faucetconfrpc_pb2_grpc.py,faucetconfrpc_pb2.py src/faucetconfrpc/*py
+PYTHONPATH=$pwd/src/faucetconfrpc:$PYTHONPATH python3 -m pytype --exclude=src/faucetconfrpc/faucetconfrpc_pb2_grpc.py --exclude=src/faucetconfrpc/faucetconfrpc_pb2.py src/faucetconfrpc/*py
+python3 -m pylint --ignore=faucetconfrpc_pb2_grpc.py,faucetconfrpc_pb2.py src/faucetconfrpc/*py
